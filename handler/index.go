e5a9249faf72dd1f25b94580cb9777b1a9774e82
shinofara <shinofara@gmail.com>
2017-02-24 20:27:16 +0900

    add schema

--- handler/index.go
+++ handler/index.go
@@ -3,15 +3,33 @@ package handler
 import (
 	"github.com/shinofara/simple-go-web-app/render"
 	"github.com/shinofara/simple-go-web-app/service"
-	"github.com/shinofara/simple-go-web-app/context"	
+	"github.com/shinofara/simple-go-web-app/context"
+	"github.com/gorilla/schema"
 	"net/http"
 )
 
+type User struct {
+	Name  string
+}
+
 func Index(rw http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		// Handle error
+	}
+	var u User
+
+	var decoder = schema.NewDecoder()
+	err = decoder.Decode(&u, r.Form)
+	if err != nil {
+		// Handle error
+	}
+	
+	
 	ctx := r.Context()
 	logger := context.MustGetLogger(ctx)
 	us := service.NewUser(ctx)
-	user, err := us.Register("test")
+	user, err := us.Register(u.Name)
 
 	re := render.New(rw, r)
 	if err != nil {
@@ -19,6 +37,7 @@ func Index(rw http.ResponseWriter, r *http.Request) {
 		re.HTML("sample", map[string]string{"name": err.Error()})
 		return
 	}
+	user.Name = u.Name
 	re.HTML("sample", map[string]string{"name": user.Name})
 	return
 }
